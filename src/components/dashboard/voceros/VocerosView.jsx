426ddd89540a3e42f646ba02efc14d5f9320c5b4
Servidor <proyectosnuevos366@gmail.com>
2026-07-13 15:26:13 -0400

    Arreglamos que la fecha de atendido no se limpiara al registrar un vocero

--- src/components/dashboard/voceros/VocerosView.jsx
+++ src/components/dashboard/voceros/VocerosView.jsx
@@ -275,7 +275,6 @@ export default function VocerosView() {
       setTelefonoVocero,
       setCorreoVocero,
       setLaboralVocero,
-      setFecha,
       setIdComuna,
       setIdCircuito,
       setIdConsejo,
